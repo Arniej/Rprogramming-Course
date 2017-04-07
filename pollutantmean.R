c3d786996f960fa92bd1328968bb27cd4b098f07
Arniej <arnie.jimenez@vel-lab.com>
2017-04-07 18:11:46 -0500

    Update pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -3,7 +3,7 @@
 pollutantmean <- function(directory, pollutant, id = 1:332){
     dat <-  numeric()
     for (i in id) {
-        select_file <- file.path(directory, paste(sprintf("%03d", i), ".csv", sep=""))
+        select_file <- file.path(directory, paste(sprintf("%03d", i), ".csv", sep="")) ##some C type code, need to work this one##
         data_subset <- read.csv(select_file, header=TRUE)
         not_NA <- !is.na(data_subset[, pollutant])
         dat <- c(dat, data_subset[not_NA, pollutant])
